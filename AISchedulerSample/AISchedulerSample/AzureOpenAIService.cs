ea1fa97a47958dc75a4f929e930366569c39268b
snehakPV <sneha.sf4154@gmail.com>
2026-01-07 16:52:48 +0530

    Removed the credentials

--- AISchedulerSample/AISchedulerSample/AzureOpenAIService.cs
+++ AISchedulerSample/AISchedulerSample/AzureOpenAIService.cs
@@ -5,11 +5,11 @@ namespace AISchedulerSample
 {
     internal class AzureOpenAIService
     {
-        internal const string endpoint = "https://mobilemaui.openai.azure.com/";
+        internal const string endpoint = "";
 
-        internal const string deploymentName = "gpt-4o";
+        internal const string deploymentName = "";
 
-        internal const string key = "6673b6975f334c79bd0db8a1cd70aa49";
+        internal const string key = "";
 
         private IChatCompletionService? chatCompletions;
 
